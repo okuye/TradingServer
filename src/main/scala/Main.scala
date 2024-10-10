e4beb39e617a2fcbfb07171392dab76ce70f0476
olakunlekuye2 <olakunle.kuye2@digital.hmrc.gov.uk>
2024-10-10 21:03:10 +0100

    Added metedata

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,6 +8,7 @@ import java.time.LocalDate
 import scala.math.Ordered.orderingToOrdered
 import scala.util.Try
 
+// Define the Trade case class
 case class Trade(
                   symbol: String,
                   date: String,
@@ -55,6 +56,35 @@ object Trade {
   implicit val encoder: JsonEncoder[Trade] = DeriveJsonEncoder.gen[Trade]
 }
 
+// Case class for response columns
+case class Column(name: String, `type`: String)
+
+object Column {
+  implicit val encoder: JsonEncoder[Column] = DeriveJsonEncoder.gen[Column]
+}
+
+// Modify DataTableResponse to use List[List[String]] for specific typing
+case class DataTableResponse(data: List[List[String]], columns: List[Column])
+
+object DataTableResponse {
+  implicit val encoder: JsonEncoder[DataTableResponse] = DeriveJsonEncoder.gen[DataTableResponse]
+}
+
+// Case class for the final response structure
+case class TradeResponse(datatable: DataTableResponse, meta: MetaData)
+
+object TradeResponse {
+  implicit val encoder: JsonEncoder[TradeResponse] = DeriveJsonEncoder.gen[TradeResponse]
+}
+
+// Case class for metadata
+case class MetaData(next_cursor_id: Option[String])
+
+object MetaData {
+  implicit val encoder: JsonEncoder[MetaData] = DeriveJsonEncoder.gen[MetaData]
+}
+
+// Case class for the container of trades
 case class Datatable(data: List[List[Any]])
 
 object Datatable {
@@ -68,12 +98,14 @@ object Datatable {
   implicit val decoder: JsonDecoder[Datatable] = DeriveJsonDecoder.gen[Datatable]
 }
 
+// Case class for the container response
 case class DatatableContainer(datatable: Datatable)
 
 object DatatableContainer {
   implicit val decoder: JsonDecoder[DatatableContainer] = DeriveJsonDecoder.gen[DatatableContainer]
 }
 
+// Function to process trades from the JSON string
 def processTrades(jsonString: String): ZIO[Any, Throwable, List[Trade]] = {
   ZIO.fromEither(jsonString.fromJson[DatatableContainer].left.map(e => new Exception(e)))
     .flatMap { container =>
@@ -83,7 +115,48 @@ def processTrades(jsonString: String): ZIO[Any, Throwable, List[Trade]] = {
     }
 }
 
+// Function to convert a list of trades to the response format
+def convertTradesToResponse(trades: List[Trade]): TradeResponse = {
+  val columns = List(
+    Column("symbol", "String"),
+    Column("date", "Date"),
+    Column("hour", "Integer"),
+    Column("openbid", "double"),
+    Column("highbid", "double"),
+    Column("lowbid", "double"),
+    Column("closebid", "double"),
+    Column("openask", "double"),
+    Column("highask", "double"),
+    Column("lowask", "double"),
+    Column("closeask", "double"),
+    Column("totalticks", "Integer")
+  )
 
+  // Convert Trade data to List[List[String]] for specific typing
+  val tradeData = trades.map { trade =>
+    List(
+      trade.symbol,
+      trade.date,
+      trade.hour.toString,
+      trade.openbid.toString,
+      trade.highbid.toString,
+      trade.lowbid.toString,
+      trade.closebid.toString,
+      trade.openask.toString,
+      trade.highask.toString,
+      trade.lowask.toString,
+      trade.closeask.toString,
+      trade.totalticks.toString
+    )
+  }
+
+  TradeResponse(
+    datatable = DataTableResponse(tradeData, columns),
+    meta = MetaData(next_cursor_id = None)
+  )
+}
+
+// Function to filter trades within a date range
 def filterTradesStream(startDate: LocalDate, endDate: LocalDate): ZStream[Any, Throwable, Trade] = {
   val path = Paths.get("src/main/resources/TradesData.json")
 
@@ -107,11 +180,10 @@ def filterTradesStream(startDate: LocalDate, endDate: LocalDate): ZStream[Any, T
   }
 }
 
-
 object Main extends ZIOAppDefault {
 
   val app = Http.collectZIO[Request] {
-    case req @ Method.GET -> Root / "trades" =>  // Replaced !! with Root
+    case req @ Method.GET -> Root / "trades" =>
       (for {
         startDate <- ZIO.fromOption(req.url.queryParams.get("startDate").flatMap(_.headOption).flatMap(s => Try(LocalDate.parse(s)).toOption))
           .orElseFail(Response.text("Missing or invalid startDate").withStatus(Status.BadRequest))
@@ -119,8 +191,8 @@ object Main extends ZIOAppDefault {
           .orElseFail(Response.text("Missing or invalid endDate").withStatus(Status.BadRequest))
         tradesStream = filterTradesStream(startDate, endDate)
         filteredTrades <- tradesStream.runCollect.map(_.toList)
-        response = Response.json(filteredTrades.toJson)
-      } yield response).catchAll { error =>
+        response = convertTradesToResponse(filteredTrades)
+      } yield Response.json(response.toJson)).catchAll { error =>
         ZIO.succeed(Response.text(s"Error: ${error.toString}").withStatus(Status.InternalServerError))
       }
   }
